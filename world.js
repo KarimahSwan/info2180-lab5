18ba00863e188454b72616225c89dd580b81ba6b
karimahswan <karimahswan@gmail.com>
2020-11-19 15:07:27 -0500

    Completed Exercise 4

--- world.js
+++ world.js
@@ -1,10 +1,35 @@
 window.onload = function() {
     var button = document.getElementById("lookup");
+    var button1 = document.getElementById("lookup1");
     button.addEventListener("click", function(e) {
         e.preventDefault();
         var country = document.getElementById("country").value;
         var httpRequest = new XMLHttpRequest();
         var url = "world.php?country=" + country;
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    var response = httpRequest.responseText;
+                    console.log(response);
+                    var inputmessage = document.getElementById("result");
+                    inputmessage.innerHTML = response;
+                } else {
+
+                    alert('There was a problem with the request.');
+                }
+            }
+        }
+        httpRequest.open('GET', url, true);
+        httpRequest.send();
+
+    });
+
+    button1.addEventListener("click", function(e) {
+        e.preventDefault();
+        var country = document.getElementById("country").value;
+        var cities = document.getElementById("country").value;
+        var httpRequest = new XMLHttpRequest();
+        var url = "world.php?country=" + country + "&context=cities";
         httpRequest.onreadystatechange = function() {
             if (httpRequest.readyState === XMLHttpRequest.DONE) {
                 if (httpRequest.status === 200) {
--- world.php
+++ world.php
@@ -16,6 +16,30 @@ $results = $querycountry->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <ul>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <!-- <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li> -->
 <?php endforeach; ?>
 </ul>
+
+<?php ?>
+  <table class = "display">
+    <caption><h2> TABLE SHOWING COUNTRIES</h2></caption>
+    <thead>
+      <tr>
+        <th class = "th">Name</th>
+        <th class = "th1">Continent</th>
+        <th class = "th2">Independence</th>
+        <th class = "th3">Name of State</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($results as $country): ?>
+        <tr>
+          <td><?php echo $country["name"]; ?></td>
+          <td><?php echo $country["continent"]; ?></td>
+          <td><?php echo $country["independence_year"]; ?></td>
+          <td><?php echo $country["head_of_state"]; ?></td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+<?php ?>
